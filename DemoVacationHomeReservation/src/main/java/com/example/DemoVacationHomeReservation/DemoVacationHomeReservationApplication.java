95bf8f40d3d0875926c4e5a62f869a605401328a
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-02 09:45:33 -0400

    Application Creation

---
+++ DemoVacationHomeReservation/src/main/java/com/example/DemoVacationHomeReservation/DemoVacationHomeReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoVacationHomeReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVacationHomeReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVacationHomeReservationApplication.class, args);
+	}
+
+}
---
+++ DemoVacationHomeReservation/src/test/java/com/example/DemoVacationHomeReservation/DemoVacationHomeReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoVacationHomeReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVacationHomeReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
